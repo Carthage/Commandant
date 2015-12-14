57b72e868d528bd08509c54ea223c91baf445a79
Syo Ikeda <suicaicoca@gmail.com>
2015-12-14 12:33:21 +0900

    Extract ArgumentType into its file

--- Commandant/Option.swift
+++ Commandant/Option.swift
@@ -92,31 +92,6 @@ extension Option: CustomStringConvertible {
 	}
 }
 
-/// Represents a value that can be converted from a command-line argument.
-public protocol ArgumentType {
-	/// A human-readable name for this type.
-	static var name: String { get }
-
-	/// Attempts to parse a value from the given command-line argument.
-	static func fromString(string: String) -> Self?
-}
-
-extension Int: ArgumentType {
-	public static let name = "integer"
-
-	public static func fromString(string: String) -> Int? {
-		return Int(string)
-	}
-}
-
-extension String: ArgumentType {
-	public static let name = "string"
-
-	public static func fromString(string: String) -> String? {
-		return string
-	}
-}
-
 // Inspired by the Argo library:
 // https://github.com/thoughtbot/Argo
 /*
