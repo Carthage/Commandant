c0352d883b41d7fc6b8f6babc8d1ecdd0d06454b
JP Simard <jp@jpsim.com>
2016-01-29 22:08:43 -0800

    point Package.swift to antitypical/Result

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Commandant",
   dependencies: [
-    .Package(url: "https://github.com/jpsim/Result.git", majorVersion: 1)
+    .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1)
   ]
 )
