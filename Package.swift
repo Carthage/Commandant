5bb41a840f12f3752de78e1bf3114a51195cec8e
Norio Nomura <norio.nomura@gmail.com>
2016-07-14 10:32:20 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Commandant",
   dependencies: [
-    .Package(url: "https://github.com/antitypical/Result.git", "3.0.0-alpha.1")
-  ],
-  testDependencies: [
+    .Package(url: "https://github.com/antitypical/Result.git", "3.0.0-alpha.1"),
     .Package(url: "https://github.com/norio-nomura/Nimble", "5.0.0-alpha.30p2"),
     .Package(url: "https://github.com/norio-nomura/Quick", "5.0.0-alpha.1"),
   ]
