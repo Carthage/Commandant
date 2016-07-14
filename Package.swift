7e6ce9ca969ba75bb76ff272e89031c86ba73913
Norio Nomura <norio.nomura@gmail.com>
2016-07-14 11:45:00 +0900

    Fix version of Quick in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/antitypical/Result.git", "3.0.0-alpha.1"),
     .Package(url: "https://github.com/norio-nomura/Nimble", "5.0.0-alpha.30p2"),
-    .Package(url: "https://github.com/norio-nomura/Quick", "5.0.0-alpha.1"),
+    .Package(url: "https://github.com/norio-nomura/Quick", "0.10.0-alpha.1"),
   ]
 )
