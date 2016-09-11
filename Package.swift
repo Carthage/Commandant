10950540cca1352c7f4cbf50337a3d65ba1d2c8b
giginet <giginet.net@gmail.com>
2016-09-12 03:02:05 +0900

    Bump up Result version to 3.0.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Commandant",
   dependencies: [
-    .Package(url: "https://github.com/antitypical/Result.git", "3.0.0-alpha.4")
+    .Package(url: "https://github.com/antitypical/Result.git", "3.0.0")
   ]
 )
