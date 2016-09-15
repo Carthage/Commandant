6855f8804f1a9dbd991dbd84ac38c96613bc6a69
Norio Nomura <norio.nomura@gmail.com>
2016-09-15 17:16:12 +0900

    Update Package.swift
    Both of Nimble and Quick are same with original of them.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name: "Commandant",
   dependencies: [
     .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
-    .Package(url: "https://github.com/norio-nomura/Nimble", "5.0.0-alpha.30p6"),
-    .Package(url: "https://github.com/norio-nomura/Quick", "0.10.0-alpha.2"),
+    .Package(url: "https://github.com/ikesyo/Nimble", "5.0.0-alpha.30.gm.candidate"),
+    .Package(url: "https://github.com/norio-nomura/Quick", "0.10.0-alpha.30.gm.candidate"),
   ]
 )
