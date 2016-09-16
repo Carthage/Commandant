00460d6b8e4e7576d4e83e02b1129e382597d484
Norio Nomura <norio.nomura@gmail.com>
2016-09-16 20:25:14 +0900

    Workaround for SR-2651

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
 import PackageDescription
 
 let package = Package(
-  name: "Commandant",
-  dependencies: [
-    .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
-    .Package(url: "https://github.com/ikesyo/Nimble", "5.0.0-alpha.30.gm.candidate"),
-    .Package(url: "https://github.com/norio-nomura/Quick", "0.10.0-alpha.30.gm.candidate"),
-  ]
+    name: "Commandant",
+    dependencies: {
+        #if os(macOS)
+            return [
+                .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
+            ]
+        #else
+            return [
+                .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
+                .Package(url: "https://github.com/ikesyo/Nimble", "5.0.0-alpha.30.gm.candidate"),
+                .Package(url: "https://github.com/norio-nomura/Quick", "0.10.0-alpha.30.gm.candidate"),
+            ]
+        #endif
+    }()
 )
