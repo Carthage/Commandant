c4141843297f0d6b9bd5f8a6f186a31a365de55b
Norio Nomura <norio.nomura@gmail.com>
2016-09-20 11:32:34 +0900

    Change tag of Nimble in Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         #else
             return [
                 .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
-                .Package(url: "https://github.com/ikesyo/Nimble", "5.0.0-alpha.30.gm.candidate"),
+                .Package(url: "https://github.com/ikesyo/Nimble", "5.0.0-alpha.30"),
                 .Package(url: "https://github.com/norio-nomura/Quick", "0.10.0-alpha.30.gm.candidate"),
             ]
         #endif
