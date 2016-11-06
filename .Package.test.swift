0f0dd8e0bae3addafb069888d720f51d61f97847
Syo Ikeda <suicaicoca@gmail.com>
2016-11-06 14:41:47 +0900

    [SwiftPM] Remove test dependencies from `Package.swift`
    We still run `swift test` on CI using separated `.Package.test.swift`.

--- .Package.test.swift
+++ .Package.test.swift
@@ -4,5 +4,7 @@ let package = Package(
     name: "Commandant",
     dependencies: [
         .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5),
+        .Package(url: "https://github.com/Quick/Quick", majorVersion: 0, minor: 10),
     ]
 )
