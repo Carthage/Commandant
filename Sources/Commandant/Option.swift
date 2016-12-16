33111abad7cc4070a683692ca23322cb70672695
Anton Domashnev <antondomashnev@gmail.com>
2016-12-16 11:06:13 +0100

    Swap the flag in split String extension; Add tests;

--- Sources/Commandant/Option.swift
+++ Sources/Commandant/Option.swift
@@ -84,15 +84,6 @@ extension Option: CustomStringConvertible {
 	}
 }
 
-extension String {
-	/// Split the string into substrings separated by the given separators.
-	internal func split(by separators: [Character] = [ ",", " " ], allowEmptySlices: Bool = true) -> [String] {
-		return characters
-				.split(omittingEmptySubsequences: allowEmptySlices, whereSeparator: separators.contains)
-				.map(String.init)
-	}
-}
-
 // Inspired by the Argo library:
 // https://github.com/thoughtbot/Argo
 /*
