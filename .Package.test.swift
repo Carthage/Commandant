0cd56446bdb8cb94acc26093c8322c47d9544fbe
Syo Ikeda <suicaicoca@gmail.com>
2017-01-04 13:33:49 +0900

    Update Quick and Nimble

--- .Package.test.swift
+++ .Package.test.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Commandant",
     dependencies: [
         .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 1, 0)..<Version(3, .max, .max)),
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5),
-        .Package(url: "https://github.com/Quick/Quick", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 1),
+        .Package(url: "https://github.com/Quick/Quick", majorVersion: 1),
     ]
 )
