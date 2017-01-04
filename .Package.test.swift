8cd702213b868233802b6fac8a92d46ad448b79e
Syo Ikeda <suicaicoca@gmail.com>
2017-01-04 13:27:31 +0900

    [SwiftPM] Accept Result 3.x versions

--- .Package.test.swift
+++ .Package.test.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Commandant",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
+        .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 1, 0)..<Version(3, .max, .max)),
         .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 0, minor: 10),
     ]
