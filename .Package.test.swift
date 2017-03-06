e1ebd6d0abfb9073b4a8669b34320eac8dbe9e7c
Syo Ikeda <suicaicoca@gmail.com>
2017-03-07 00:10:05 +0900

    [Package.swift] Add `.git` suffix to dependency urls consistently

--- .Package.test.swift
+++ .Package.test.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Commandant",
     dependencies: [
         .Package(url: "https://github.com/antitypical/Result.git", versions: Version(3, 2, 1)..<Version(3, .max, .max)),
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6),
-        .Package(url: "https://github.com/Quick/Quick", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6),
+        .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
     ]
 )
