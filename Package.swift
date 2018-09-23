9b79808a16cf4b25f52c3e7b309a72cf095f44ab
Sho Ikeda <suicaicoca@gmail.com>
2018-09-24 01:59:54 +0900

    Update Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/antitypical/Result.git", from: "4.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", from: "1.3.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "7.1.2"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.3.0"),
     ],
     targets: [
         .target(name: "Commandant", dependencies: ["Result"]),
